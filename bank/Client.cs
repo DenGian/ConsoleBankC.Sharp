9b3cc09e9045e840deae0d344ea007aff0061a17
DenGian <mondelaers.ian@gmail.com>
2022-09-15 21:40:45 +0200

    error fixing

--- bank/Client.cs
+++ bank/Client.cs
@@ -1,6 +1,16 @@
-namespace DefaultNamespace;
+namespace ClientAccount;
 
 public class Client
 {
-    
+    // defining properties
+    protected int Id;
+    protected string Name;
+    protected DateTime DateJoined;
+    // constructor that takes multiple arguments
+    public Client(int id, string name, DateTime dateJoined)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.DateJoined = dateJoined;
+    }
 }
