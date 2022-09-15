ae7d54e8e6e1b437f06656cd93592bb40e0b3f9f
DenGian <mondelaers.ian@gmail.com>
2022-09-15 21:40:13 +0200

    error fixing

--- bank/BankAccount.cs
+++ bank/BankAccount.cs
@@ -1,58 +1,50 @@
 namespace BankAccount;
+using ClientAccount;
 
 public class Account
 {
-public enum Type 
-{
-checkingsAccount, 
-savingsAccount
-}
-    public Client client;
-    public int balance = 0;
-    public Type type;
+    public enum Type
+    {
+        CheckingsAccount,
+        SavingsAccount
+    }
 
-public Account(Client client, int balance, int type) {
-this.balance += balance;
-this.client = client;
-this.type = typeSelector(type);
-}
-public Type typeSelector(int selector)
-{
-if(selector == 1)
-{
-return type = Type.checkingsAccount;
-}
-else if (selector == 2)
-{
-return type = Type.savingingsAccount;
-}
-else
-{
-return type = Type.checkingsAccount;
-}
-public int checkBalance()
-{
-return this.balance;
-}
-public int withdraw(int amountWithdraw)
-{
-if (amount<=0)
-{
-throw new Exception("You need to have a positive number please.");
-}
-if (this.balance - amount < 0)
-{
-throw new Exception("You don't have sufficient funds");
-}
-return this.balance -= amount;
-}
-public int Deposit (int amount)
-{
-if (amount <= 0) 
-{
-throw new Exception("Amount has to be more than +0");
-}
-return this.balance += amount;
-}
-}
+// defining properties
+    protected Client client;
+    protected int balance;
+    protected Type type;
+
+// constructor
+    public Account(Client client, int balance, int type)
+    {
+        this.balance += balance;
+        this.client = client;
+        this.type = TypeSelector(type);
+    }
+
+//methods
+    public Type TypeSelector(int selector)
+    {
+        if (selector == 1)
+            return type = Type.CheckingsAccount;
+        if (selector == 2)
+            return type = Type.SavingsAccount;
+        return type = Type.CheckingsAccount;
+    }
+    public int CheckBalance()
+    {
+        return balance;
+    }
+    public int Withdraw(int amountWithdraw)
+    {
+        if (amountWithdraw <= 0) throw new Exception("You need to have a positive number please.");
+        if (balance - amountWithdraw < 0) throw new Exception("You don't have sufficient funds");
+        return balance -= amountWithdraw;
+    }
+
+    public int Deposit(int amount)
+    {
+        if (amount <= 0) throw new Exception("Amount has to be more than +0");
+        return balance += amount;
+    }
 }
